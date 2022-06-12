5ea18fa5b5b2f50c8d7a87d1ba8985d3cb43318a
Alys <alice.g.cooper@gmail.com>
2022-06-12 16:40:01 -0400

    Add anti-pluralizer logic for 0 or less than 0

--- js/pluralizer.js
+++ js/pluralizer.js
@@ -2,7 +2,9 @@ let thing = 'cat';
 let count = 1;
 
 function pluralizer(thing,count) {
-    if (count > 1) {
+    if (count < 1) {
+        console.log(`There aren't any ${thing}s here!`)
+    } else if (count > 1) {
         console.log(`${thing}s`);
     } else {
         console.log(`${thing}`)
